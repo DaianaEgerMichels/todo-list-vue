24afc60b3bd09c9113e8cd302e9ee74eda102428
Daiana Michels <daiana.michels@pixeon.com>
2022-09-26 10:51:08 -0300

    fetches data from database.json

--- src/App.vue
+++ src/App.vue
@@ -28,10 +28,35 @@ import TodoSpinner from './components/TodoSpinner.vue';
 import TodoFormAdd from './components/TodoFormAdd.vue';
 import TodoItems from './components/TodoItems.vue';
 import TodoEmpty from './components/TodoEmpty.vue';
+import axios from 'axios'
 
 export default {
     name: "App",
-    components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty }
+    components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty },
+
+    data() {
+        return {
+            todos: []
+        }
+    },
+
+    //opção 01:
+    // async created() {
+    //     this.todos = await axios.get('http://localhost:3000/todos')
+    //         .then((response)=>{
+    //             response.data;
+    //         })
+    // }
+
+    //opção 02:
+    created() {
+        axios.get('http://localhost:3000/todos')
+            .then((response)=>{
+                this.todos= response.data;
+            })
+    }
+
+
 }
 </script>
 
