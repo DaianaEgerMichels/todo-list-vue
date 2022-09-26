2f069e37411a003de4cedfffaf09a27ef3a440d8
Daiana Michels <daiana.michels@pixeon.com>
2022-09-26 15:12:32 -0300

    configures addition of new tasks

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,32 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    todos: []
+    todos: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
     storeTodos(state, payload) {
-      state.todos = payload
-    }
+      state.todos = payload;
+    },
+    storeTodo(state, payload) {
+      state.todos.push(payload);
+    },
   },
   actions: {
+    //retorna promises
+    getTodos({ commit }) {
+      return axios.get("http://localhost:3000/todos").then((response) => {
+        commit("storeTodos", response.data);
+      });
+    },
+
+    addTodo({ commit }, data) {
+      return axios.post("http://localhost:3000/todos", data).then((response) => {
+        commit("storeTodo", response.data);
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
